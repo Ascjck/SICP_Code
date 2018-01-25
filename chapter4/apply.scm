a59b1f7da5093fd697645332a6e2a6223539447a
Ascjck <Ascjck@outlook.com>
2018-01-25 23:45:20 +0800

    modified apply.scm eval.scm add 6 new files

--- chapter4/apply.scm
+++ chapter4/apply.scm
@@ -1,7 +1,21 @@
+(load "primitive-procedure.scm")
+(load "apply-primitive-procedure.scm")
+(load "compound-procedure.scm")
+(load "eval-sequence.scm")
+(load "procedure-body.scm")
+(load "extend-environment.scm")
+(load "procedure-parameters.scm")
+(load "procedure-environment.scm")
+
 (define (apply procedure arguments)
 	(cond ((primitive-procedure? procedure)
 		   (apply-primitive-procedure procedure arguments))
 		  ((compound-procedure? procedure)
-		   (eval-sequence (procedure-body procedure) (extend-environment (procedure-parameters procedure) arguments (procedure-environment procedure)))
+		   (eval-sequence 
+			(procedure-body procedure) 
+			(extend-environment 
+				(procedure-parameters procedure) 
+				arguments 
+				(procedure-environment procedure)))
 		  (else
 		   (error "Unknown procedure type -- APPLY" procedure)))))
